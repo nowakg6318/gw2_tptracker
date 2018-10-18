a80dd3bf590c9bb8ab6ebd9358425c866090a652
nowakg <gabemnowak@gmail.com>
2018-10-17 20:25:43 -0600

    Added easy crafting item list access.

--- app/routes.py
+++ app/routes.py
@@ -5,17 +5,6 @@ from app import app
 
 from app.webfunctions import ScrubUserText, FindItemNumber, ProcessItemName, GetMarketData, CalculateMarketEstimates
 
-@app.route('/_gettpdata')
-def GetTPData():
-    market_list = GetMarketData(session['items_dict'])
-    market_dict = CalculateMarketEstimates(market_list, session['items_dict'])
-    return(jsonify(market_dict))
-
-
-@app.route('/market_data', methods=['GET'])
-def MarketDataPage():
-    return(render_template('tp_table.html'))
-
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
@@ -45,7 +34,10 @@ def BadItemNamePage():
 
 @app.route('/calculations', methods=['GET', 'POST'])
 def CalculateArbitrage():
-    item_list = ScrubUserText(request.args['user_data'])
+    if request.args['user_data'] != '':
+      item_list = ScrubUserText(request.args['user_data'])
+    else:
+      item_list = crafting_item_list
     if not item_list:
         return(redirect(url_for('BadCharPage')))
     item_id_list = []
@@ -61,51 +53,63 @@ def CalculateArbitrage():
     return(redirect(url_for('MarketDataPage')))
 
 
-  # id_dict = {24295: 'Vial of Powerful Blood',
-  #            24294: 'Vial of Potent Blood',  
-  #            24293: 'Vial of Thick Blood',
-  #            24292: 'Vial of Blood',
-  #            24291: 'Vial of Thin Blood',
-  #            24290: 'Vial of Weak Blood',
-  #            24283: 'Powerful Venom Sac',
-  #            24282: 'Potent Venom Sac',
-  #            24281: 'Full Venom Sac',
-  #            24280: 'Venom Sac',
-  #            24279: 'Small Venom Sac',
-  #            24278: 'Tiny Venom Sac',
-  #            24296: 'Tiny Totem',
-  #            24297: 'Small Totem',
-  #            24298: 'Totem',
-  #            24363: 'Engraved Totem',
-  #            24299: 'Intricate Totem',
-  #            24300: 'Elaborate Totem',
-  #            24284: 'Tiny Scale',
-  #            24285: 'Small Scale',
-  #            24286: 'Scale',
-  #            24287: 'Smooth Scale',
-  #            24288: 'Large Scale',
-  #            24289: 'Armored Scale',
-  #            24352: 'Tiny Fang',
-  #            24353: 'Small Fang',
-  #            24354: 'Fang',
-  #            24355: 'Sharp Fang',
-  #            24356: 'Large Fang',
-  #            24357: 'Vicious Fang',
-  #            24272: 'Pile of Glittering Dust',
-  #            24273: 'Pile of Shimmering Dust',
-  #            24274: 'Pile of Radiant Dust',
-  #            24275: 'Pile of Luminous Dust',
-  #            24276: 'Pile of Incandescent Dust',
-  #            24277: 'Pile of Crystalline Dust',
-  #            24346: 'Tiny Claw',
-  #            24347: 'Small Claw',
-  #            24348: 'Claw',
-  #            24349: 'Sharp Claw',
-  #            24350: 'Large Claw',
-  #            24351: 'Vicious Claw',
-  #            24342: 'Bone Chip',
-  #            24343: 'Bone Shard',
-  #            24344: 'Bone',
-  #            24345: 'Heavy Bone',
-  #            24341: 'Large Bone',
-  #            24358: 'Ancient Bone'}
+@app.route('/_gettpdata')
+def GetTPData():
+    market_list = GetMarketData(session['items_dict'])
+    market_dict = CalculateMarketEstimates(market_list, session['items_dict'])
+    return(jsonify(market_dict))
+
+
+@app.route('/market_data', methods=['GET'])
+def MarketDataPage():
+    return(render_template('tp_table.html'))
+
+
+crafting_item_list = (['Vial of Powerful Blood',
+           'Vial of Potent Blood',  
+           'Vial of Thick Blood',
+           'Vial of Blood',
+           'Vial of Thin Blood',
+           'Vial of Weak Blood',
+           'Powerful Venom Sac',
+           'Potent Venom Sac',
+           'Full Venom Sac',
+           'Venom Sac',
+           'Small Venom Sac',
+           'Tiny Venom Sac',
+           'Tiny Totem',
+           'Small Totem',
+           'Totem',
+           'Engraved Totem',
+           'Intricate Totem',
+           'Elaborate Totem',
+           'Tiny Scale',
+           'Small Scale',
+           'Scale',
+           'Smooth Scale',
+           'Large Scale',
+           'Armored Scale',
+           'Tiny Fang',
+           'Small Fang',
+           'Fang',
+           'Sharp Fang',
+           'Large Fang',
+           'Vicious Fang',
+           'Pile of Glittering Dust',
+           'Pile of Shimmering Dust',
+           'Pile of Radiant Dust',
+           'Pile of Luminous Dust',
+           'Pile of Incandescent Dust',
+           'Pile of Crystalline Dust',
+           'Tiny Claw',
+           'Small Claw',
+           'Claw',
+           'Sharp Claw',
+           'Large Claw',
+           'Vicious Claw',
+           'Bone Chip',
+           'Bone Shard',
+           'Bone',
+           'Heavy Bone',
+           'Large Bone',
+           'Ancient Bone'])
